8b53a36706a0382ffc3e565fca26bd841d87ee4e
thegrey007 <shreyaramanujam03@gmail.com>
2024-11-29 19:16:35 +0530

    merging

--- applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/opt/single_gpu/run_125m.sh
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/opt/single_gpu/run_125m.sh
@@ -20,8 +20,14 @@ fi
 mkdir -p $OUTPUT
 
 deepspeed --num_gpus 1 main.py \
+<<<<<<< HEAD
    --actor_model_name_or_path $ACTOR_MODEL_PATH --fact_critic_model_name_or_path $CRITIC_MODEL_PATH --gen_critic_model_name_or_path $CRITIC_MODEL_PATH \
    --actor_zero_stage $ACTOR_ZERO_STAGE --critic_zero_stage $CRITIC_ZERO_STAGE \
    --num_padding_at_beginning 1 --gradient_accumulation_steps 2 \
+=======
+   --actor_model_name_or_path $ACTOR_MODEL_PATH --critic_model_name_or_path $CRITIC_MODEL_PATH \
+   --actor_zero_stage $ACTOR_ZERO_STAGE --critic_zero_stage $CRITIC_ZERO_STAGE \
+   --num_padding_at_beginning 1 --gradient_accumulation_steps 2 --per_device_training_batch_size 4 --per_device_generation_batch_size 4 \
+>>>>>>> d6e14d5814c5c3d9406f3db98b08d624192ea9aa
    --deepspeed --actor_lora_dim 128 --enable_hybrid_engine --actor_gradient_checkpointing --actor_dropout 0.0 \
    --output_dir $OUTPUT &> $OUTPUT/training.log
